abf028fe66ecd638556b278e9ec3858f25dc2952
lesley <lesleybanadzem2017@gmail.com>
2019-12-27 11:54:26 -0800

    Define a graphql schema for books

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
-const express = require('express')
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const app = express();
 const PORT = process.env.PORT || 4040;
 app.use(express.json());
 
+//this is the super charged endpoint  
+app.use('/graphql', graphqlHTTP({}))
 
 
-app.listen(PORT, () => console.log(`App listening to port 4040`))
+
+app.listen(PORT, () => console.log(`App listening to port http://localhost:${PORT}`))
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,1 +1,19 @@
 const  graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+//define schema
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: {
+            type: GraphQLString
+        },
+        name: {
+            type: GraphQLString
+        },
+        genre: {
+            type: GraphQLString
+        }
+     })
+})
