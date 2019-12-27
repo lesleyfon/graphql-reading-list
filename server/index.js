ac7db0211eb03bae13a5e3df8325a3ee5e8def54
lesley <lesleybanadzem2017@gmail.com>
2019-12-27 12:18:57 -0800

    Impoer the schema into index.js and created a dummuy data to test if the query works

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema')
 const app = express();
 const PORT = process.env.PORT || 4040;
 app.use(express.json());
 
 //this is the super charged endpoint  
-app.use('/graphql', graphqlHTTP({}))
+app.use('/graphql', graphqlHTTP({
+    schema
+}))
 
 
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,14 @@
 const  graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+// dummy data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
 //define schema
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -19,6 +26,7 @@ const BookType = new GraphQLObjectType({
 });
 
 // RootQuery
+//How we Jump into a graph
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -31,7 +39,7 @@ const RootQuery = new GraphQLObjectType({
             },
             resolve(parent, args){
                 //code to get data from db
-
+                return _.find(books, { id: args.id});
             }
         }
     }
