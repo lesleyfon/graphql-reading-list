57b198d8b3238224fd33e46fe81ddd672e1682ad
lesley <lesleybanadzem2017@gmail.com>
2019-12-28 12:46:59 -0800

    Created an intance of mongodb and connected app to the m ongodb using mongoose

--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
+require('dotenv').config()
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema')
 const app = express();
 const PORT = process.env.PORT || 4040;
+const MONGO_URL = process.env.MONGO_URL;
+const mongoose = require('mongoose');
 app.use(express.json());
+
+//connect to the mongo db
+mongoose.connect(
+    MONGO_URL, 
+    { 
+        useNewUrlParser: true ,
+        useUnifiedTopology: true
+});
+
+mongoose.connection.once('open', ()=> {
+    console.log('connected to db')
+})
+
 
 //this is the super charged endpoint  
 app.use('/graphql', graphqlHTTP({
-    schema
+    schema,
+    graphiql: true
 }))
 
 
