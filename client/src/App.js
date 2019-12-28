9e7f969d03a92027db79fb80127d8fbcfcd02646
lesley <lesleybanadzem2017@gmail.com>
2019-12-28 15:17:54 -0800

    Created components and added some basic styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import BookList from './components/BookList'
 
 function App() {
     return (
-        <div>
-            <h1>Client </h1>
+        <div className='main'>
+            <h1>Reading List... </h1>
+            <BookList />
         </div>
     )
 }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect}from 'react'
+
+function BookList() {
+    return (
+        <div>
+            <ul id='book-list'>
+                <li>New Book</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BookList
