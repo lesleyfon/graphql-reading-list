1c9faedecc70d13db05eae19f550399ae3e2a572
lesley <lesleybanadzem2017@gmail.com>
2020-12-11 08:13:43 -0800

    1. Added jsx for deleting a book in the BookDetails component 2. Styled the JSX

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,44 +1,43 @@
-import React, {}from 'react';
-import { graphql } from 'react-apollo';
-import {getBookQuery} from "./../queries/queries"
+import React from "react";
+import { graphql } from "react-apollo";
+import { getBookQuery } from "./../queries/queries";
 
+// Delete Svg
+import DeleteSVG from "./../assets/trash.svg";
 
 function BookDetails(props) {
-    const {book} = props.data;
-    const displayBookDetails = () => {
-        if(book){
-            return (
-                <div>
-                    <h2>{book.name}</h2>
-                    <p>{book.genre}</p>
-                    <p>{book.author.name}</p>
-                    <p>All books By {book.author.name}</p>
-                    <ul className='other-books'>
-                        {book.author.book.map(item => <li key={item.id}>{item.name}</li>)}
-                    </ul>
-                </div>
-            )
-        }else{
-            return (
-                <div>
-                    No book Selected
-                </div>
-            )
-        }
-    }
-    return (
-        <div id='book-details'>
-            {displayBookDetails()}
-        </div>
-    )
+	const { book } = props.data;
+	const displayBookDetails = () => {
+		if (book) {
+			return (
+				<div>
+					<h2>{book.name}</h2>
+					<p>{book.genre}</p>
+					<p>{book.author.name}</p>
+					<p>All books By {book.author.name}</p>
+					<ul className="other-books">
+						{book.author.book.map((item) => (
+							<li key={item.id}>{item.name}</li>
+						))}
+					</ul>
+					<div className="delete-container">
+						<img src={DeleteSVG} alt="Delete Book" />
+					</div>
+				</div>
+			);
+		} else {
+			return <div>No book Selected</div>;
+		}
+	};
+	return <div id="book-details">{displayBookDetails()}</div>;
 }
 
 export default graphql(getBookQuery, {
-    options: (props) => {
-        return{
-            variables: {
-                id: props.bookId
-            }
-        }
-    }
-})(BookDetails)
+	options: (props) => {
+		return {
+			variables: {
+				id: props.bookId,
+			},
+		};
+	},
+})(BookDetails);
